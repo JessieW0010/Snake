4ffb9ad44aae3b3b9b0eed37fd82e1d3bba92539
Jessie Wang <wangj.5415@gmail.com>
2019-07-03 18:39:02 +0000

    added stdin and callback funciton to take in and respond to user key input

--- play.js
+++ play.js
@@ -1,4 +1,23 @@
 const connect = require("./client");
 
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true); //read from key without pressing enter
+  stdin.setEncoding('utf8');  //read text based on unicode
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+}
+
+const handleUserInput = function(key) {
+  if (key === "\u0003") { //ctrl + c
+    process.exit();
+  }
+}
+
 console.log('Connecting ...');
 connect();
